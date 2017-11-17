1281add5df5c0c4e3fe9cd27b342ce4ede4ca4aa
weiman <weiman@happyjuzi.com>
2017-11-17 12:24:14 +0800

    修改文件

---
+++ TestMJ/TestMJ/Classes/DownDefaultController.h
@@ -0,0 +1,13 @@
+//
+//  DownDefaultController.h
+//  TestMJ
+//
+//  Created by weiman on 2017/11/17.
+//  Copyright © 2017年 weiman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DownDefaultController : UIViewController
+
+@end
---
+++ TestMJ/TestMJ/Classes/DownDefaultController.m
@@ -0,0 +1,37 @@
+//
+//  DownDefaultController.m
+//  TestMJ
+//
+//  Created by weiman on 2017/11/17.
+//  Copyright © 2017年 weiman. All rights reserved.
+//
+
+#import "DownDefaultController.h"
+
+@interface DownDefaultController ()
+
+@end
+
+@implementation DownDefaultController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
